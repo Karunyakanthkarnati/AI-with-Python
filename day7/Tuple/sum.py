99ac4aaabd4b47a384db6220c1620d3c8e9ee94a
Karunyakanthkarnati <23pa1a05a6@vishnu.edu.in>
2026-02-19 19:08:09 +0530

    Create sum.py to compute sum of input numbers
    Add a script to calculate the sum of user-input numbers as a tuple.

---
+++ day7/Tuple/sum.py
@@ -0,0 +1,8 @@
+nums = tuple(map(int, input("Enter numbers: ").split()))
+print("Sum:", sum(nums))
+ (or)
+num = tuple(map(int,input("Enter Numbers : ").split()))
+sum = 0
+for x in num:
+    sum = sum + x
+print(sum)
